c0277b43b37bea5b739e3696eca3c118db45bc86
DESKTOP-F4HA034\Nathaniel <ny66@drexel.edu>
2018-05-08 00:31:21 -0400

    no message

---
+++ scripts/monthly.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+     getDate()
+ }
+
+ function getDate(){
+     var current = new Date();
+     var d = current.getDate();
+     var m = current.getMonth();
+     var y = current.getFullYear();
+     console.log(y + "-" + (m + 1) + "-" + d)
+     return y + "-" + m + "-" + d
+ }
